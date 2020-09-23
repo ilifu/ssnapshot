e441680ee24a1b94dbdddf15b46ffa23e4ff8945
Dane Kennedy <kennedy.dane@gmail.com>
2020-09-23 14:04:59 +0200

    initial code

---
+++ tests/test_ssnapshot.py
@@ -0,0 +1,11 @@
+from ssnapshot.ssnapshot import (
+    create_job_summaries,
+    create_job_detail_summary,
+    create_partition_summary,
+    get_sinfo,
+    get_squeue,
+    get_sstat,
+)
+
+def test_get_sstat():
+    assert False
