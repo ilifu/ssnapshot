89d3028c1ac89a2997c610baab806166ef29da4f
Dane Kennedy <kennedy.dane@gmail.com>
2020-09-29 21:54:55 +0200

    Change version info

--- setup.py
+++ setup.py
@@ -1,11 +1,11 @@
 import setuptools
 
-with open('README.md', 'r') as fh:
+with open('README.pypi.md', 'r') as fh:
     long_description = fh.read()
 
 setuptools.setup(
     name='ssnapshot',  # Replace with your own username
-    version='0.0.1',
+    version='0.0.2a1',
     author='Dane Kennedy',
     author_email='dane@idia.ac.za',
     description='A utility to generate a snapshot of slurm\'s state',
